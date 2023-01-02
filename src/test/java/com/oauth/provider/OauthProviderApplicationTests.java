59f9dbff72809292b5cdb80774cdfb3de99881dc
bobaejeong <jbb@kanak.co.kr>
2023-01-02 18:38:03 +0900

    인증서버 셋팅

---
+++ src/test/java/com/oauth/provider/OauthProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
